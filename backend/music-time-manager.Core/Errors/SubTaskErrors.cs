d2d9a34157a6cd346c4a4d1f1a352badef3e1dfe
Cheerful <yura08az@gmail.com>
2026-07-11 13:54:21 +0300

    feat: add SubTaskErrors

---
+++ backend/music-time-manager.Core/Errors/SubTaskErrors.cs
@@ -0,0 +1,13 @@
+﻿namespace music_time_manager.Core.Errors;
+
+public static class SubTaskErrors 
+{
+    private static class Codes
+    {
+        public const string InvalidTitle = "Task.InvalidTtile";
+    } 
+    
+    public static Error InvalidTitle(string title) =>
+        new Error(Codes.InvalidTitle, 
+            $"Title '{title}' is invalid.");
+}
