aa44fa9636bb832fac094598265b9270108e9bda
Cheerful <yura08az@gmail.com>
2026-07-11 13:00:25 +0300

    feat: create User core model

---
+++ backend/music-time-manager.Core/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using music_time_manager.Core.Errors.User;
+using music_time_manager.Core.Result;
+
+namespace music_time_manager.Core.Models;
+
+public class User
+{
+    public const int MAX_USERNAME_LENGTH = 100;
+    private User(Guid id, string username, string passwordHash)
+    {
+        Id = id;
+        UserName = username;
+        PasswordHash = passwordHash;
+    }
+    public Guid Id { get; private set; }
+    public string UserName  { get; private set; }
+    public string PasswordHash {get; private set;}
+    
+    public static ResultT<User> Create(string username, string passwordHash)
+    {
+        
+        if (string.IsNullOrWhiteSpace(username) || username.Length > MAX_USERNAME_LENGTH)
+        {
+            return ResultT<User>.Failures([UserErrors.InvalidUsername(username)]);
+        }
+
+        var user = new User(
+            id: Guid.NewGuid(),
+            username: username,
+            passwordHash: passwordHash);
+        
+        return ResultT<User>.Success(user);
+    }
+}
