5e75700e8905f6a08732e72fa1471718c79e1885
Cheerful <yura08az@gmail.com>
2026-07-11 12:48:53 +0300

    feat: add Result and ResultT

---
+++ backend/music-time-manager.Core/Result/Result.cs
@@ -0,0 +1,24 @@
+﻿using music_time_manager.Core.Errors;
+
+namespace music_time_manager.Core.Result;
+
+public class Result
+{
+    protected Result(bool isSuccess, List<Error> errors)
+    {
+        if (isSuccess && errors.Count != 0 ||
+            !isSuccess && errors.Count == 0)
+        {
+            throw new ArgumentException("Invalid error", nameof(errors));
+        }
+        
+        IsSuccess = isSuccess;
+        Errors = errors;
+    }
+    private bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public List<Error> Errors { get; }
+
+    public static Result Success => new Result(true, []);
+    public static Result Failures(List<Error> errors) => new Result(false, errors); 
+}
---
+++ backend/music-time-manager.Core/Result/ResultT.cs
@@ -0,0 +1,19 @@
+﻿using music_time_manager.Core.Errors;
+
+namespace music_time_manager.Core.Result;
+
+public class ResultT<TValue> : Result
+{
+    private readonly TValue? _value;
+
+    private ResultT(TValue value) 
+        : base(true, [])
+    {
+        _value = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    private ResultT(List<Error> errors) : base(false, errors) { }
+
+    public new static ResultT<TValue> Success(TValue value) => new(value);
+    public new static ResultT<TValue> Failures(List<Error> errors) => new (errors);
+}
