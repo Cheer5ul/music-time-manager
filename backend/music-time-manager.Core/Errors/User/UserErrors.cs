8efd8bf6f6bd78d4458c4a46ec4c73df6fccf90b
Cheerful <yura08az@gmail.com>
2026-07-11 13:00:08 +0300

    feat: add UserErrors

---
+++ backend/music-time-manager.Core/Errors/User/UserErrors.cs
@@ -0,0 +1,12 @@
+﻿namespace music_time_manager.Core.Errors.User;
+
+public static class UserErrors
+{
+    private static class Codes
+    {
+        public const string InvalidUsername = "User.InvalidName";
+    }
+    public static Error InvalidUsername(string username) => 
+        new Error(Codes.InvalidUsername,
+            $"Username '{username}' is invalid."); 
+}
