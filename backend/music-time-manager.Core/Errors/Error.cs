e27fae7a29aa1fbc822a5d7bc96805959cbc6aaa
Cheerful <yura08az@gmail.com>
2026-07-11 12:49:06 +0300

    feat: add base Error

---
+++ backend/music-time-manager.Core/Errors/Error.cs
@@ -0,0 +1,15 @@
+﻿namespace music_time_manager.Core.Errors;
+
+public sealed class Error
+{
+    public Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+    
+    public string Code { get; }
+    public string Message { get; }
+    
+    public static Error None => new Error(string.Empty, string.Empty); 
+}
