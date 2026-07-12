cf31b93a685edbce1d1bcb15500da9b015252fcf
Cheerful <yura08az@gmail.com>
2026-07-12 19:14:59 +0300

    feat: add UserConfiguration

---
+++ backend/music-time-manager.Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using music_time_manager.Core.Models;
+using music_time_manager.Persistence.Entities;
+
+namespace music_time_manager.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<UserEntity>
+{
+    public void Configure(EntityTypeBuilder<UserEntity> builder)
+    {
+        builder.HasKey(user => user.Id);
+        
+        builder.Property(user => user.UserName)
+            .HasColumnName(nameof(UserEntity.UserName))
+            .HasMaxLength(User.MAX_USERNAME_LENGTH)
+            .IsRequired();
+        
+        builder.Property(user => user.PasswordHash)
+            .HasColumnName(nameof(UserEntity.PasswordHash))
+            .IsRequired();
+    }
+}
