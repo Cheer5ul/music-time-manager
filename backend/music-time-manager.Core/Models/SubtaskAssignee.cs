4b40a55dae9cc1825d3d767e84a912cf0c7c5290
Cheerful <yura08az@gmail.com>
2026-07-12 18:06:15 +0300

    fix: add Id property

--- backend/music-time-manager.Core/Models/SubtaskAssignee.cs
+++ backend/music-time-manager.Core/Models/SubtaskAssignee.cs
@@ -2,16 +2,18 @@
 
 public class SubtaskAssignee
 {
-    public SubtaskAssignee(Guid subTaskId, Guid userId)
+    public SubtaskAssignee(Guid id, Guid subTaskId, Guid userId)
     {
+        Id = id;
         SubtaskId = subTaskId;
         UserId = userId;
     }
+    public Guid Id {get; private set;}
     public Guid SubtaskId { get; private set; }
     public Guid UserId { get; private set; }
 
-    public static SubtaskAssignee Reconstitute(Guid taskId, Guid userId)
+    public static SubtaskAssignee Reconstitute(Guid id, Guid taskId, Guid userId)
     {
-        return new SubtaskAssignee(taskId, userId);
+        return new SubtaskAssignee(id, taskId, userId);
     }
 }
--- backend/music-time-manager.Core/Models/TaskAssignee.cs
+++ backend/music-time-manager.Core/Models/TaskAssignee.cs
@@ -2,16 +2,18 @@
 
 public class TaskAssignee
 {
-    public TaskAssignee(Guid taskId, Guid userId)
+    public TaskAssignee(Guid id, Guid taskId, Guid userId)
     {
+        Id = id;
         TaskId = taskId;
         UserId = userId;        
     }
+    public Guid Id {get; private set;}
     public Guid TaskId { get; private set; }
     public Guid UserId { get; private set; }
 
-    public static TaskAssignee Reconstitute(Guid taskId, Guid userId)
+    public static TaskAssignee Reconstitute(Guid id, Guid taskId, Guid userId)
     {
-        return new TaskAssignee(taskId, userId);
+        return new TaskAssignee(id, taskId, userId);
     }
 }
