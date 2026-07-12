700c8c890f11d4d5dc4dfc4af250f15d15eb535a
Cheerful <yura08az@gmail.com>
2026-07-12 19:15:20 +0300

    wip: adding DbContext

---
+++ backend/music-time-manager.Persistence/MusicTimeManagerDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using music_time_manager.Persistence.Configurations;
+using music_time_manager.Persistence.Entities;
+
+namespace music_time_manager.Persistence;
+
+public class MusicTimeManagerDbContext : DbContext
+{
+    public MusicTimeManagerDbContext(DbContextOptions<MusicTimeManagerDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    public MusicTimeManagerDbContext() { }
+    
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<TaskEntity> Tasks { get; set; }
+    public DbSet<SubtaskEntity> Subtasks { get; set; }
+    public DbSet<TaskAssigneeEntity> TaskAssignees { get; set; }
+    public DbSet<SubtaskAssigneeEntity> SubtaskAssignees { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new TaskEntityConfiguration());
+    }
+}
