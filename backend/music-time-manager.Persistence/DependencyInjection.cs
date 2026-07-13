c0d12087cc326643cad2d88073052ddc5ef3a1e7
Cheerful <yura08az@gmail.com>
2026-07-13 21:48:21 +0300

    wip: add DI

---
+++ backend/music-time-manager.Persistence/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace music_time_manager.Persistence;
+
+public class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString(nameof(MusicTimeManagerDbContext));
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException(
+                "Connection string is empty or null");
+        }
+
+        services.AddDbContext<MusicTimeManagerDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString(nameof(MusicTimeManagerDbContext)));
+        });
+
+        // add services 
+        // services.AddScoped<>();
+        
+        return services;
+    }
+}
