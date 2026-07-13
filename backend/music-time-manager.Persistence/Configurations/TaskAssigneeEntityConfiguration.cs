9f5b04b51b6c82e5ce6bf2c446fcd9d1b8497be5
Cheerful <yura08az@gmail.com>
2026-07-13 21:38:35 +0300

    feat: add TaskAssigneeEntityConfiguration

---
+++ backend/music-time-manager.Persistence/Configurations/TaskAssigneeEntityConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using music_time_manager.Persistence.Entities;
+
+namespace music_time_manager.Persistence.Configurations;
+
+public class TaskAssigneeEntityConfiguration : IEntityTypeConfiguration<TaskAssigneeEntity>
+{
+    public void Configure(EntityTypeBuilder<TaskAssigneeEntity> builder)
+    {
+        builder.HasKey(ta => new {ta.TaskId, ta.UserId});
+        
+        builder.HasOne(ta => ta.Task)
+            .WithMany(task => task.TaskAssignees)
+            .HasForeignKey(ta => ta.TaskId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(ta => ta.User)
+            .WithMany(user => user.TaskAssignees)
+            .HasForeignKey(ta => ta.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
+
