75d5f5aa8b026017f433657dcc7cba15c534a367
Cheerful <yura08az@gmail.com>
2026-07-13 21:50:05 +0300

    feat: add Db initializer

---
+++ backend/music-time-manager.Persistence/DbInitializer.cs
@@ -0,0 +1,9 @@
+﻿namespace music_time_manager.Persistence;
+
+public class DbInitializer
+{
+    public static async Task InitializeAsync(MusicTimeManagerDbContext dbContext)
+    {
+        await dbContext.Database.EnsureCreatedAsync();
+    }
+}
