46f9c5cb2180e68a1ff9285c3ffbacbdb2b5e442
Cheerful <yura08az@gmail.com>
2026-07-13 20:53:22 +0300

    feat: add SubtaskConfiguration

---
+++ backend/music-time-manager.Persistence/Configurations/SubtaskEntityConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using music_time_manager.Persistence.Entities;
+using Task = music_time_manager.Core.Models.Task;
+
+namespace music_time_manager.Persistence.Configurations;
+
+public class SubtaskEntityConfiguration : IEntityTypeConfiguration<SubtaskEntity>
+{
+    public void Configure(EntityTypeBuilder<SubtaskEntity> builder)
+    {
+        builder.HasKey(subtask => subtask.Id);
+
+        builder.Property(subtask => subtask.Title)
+            .HasMaxLength(Task.MAX_TITLE_LENGTH)
+            .IsRequired();
+
+        builder.Property(subtask => subtask.Status)
+            .HasConversion<string>()
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.HasOne(subtask => subtask.Task)
+            .WithMany(task => task.SubTasks)
+            .HasForeignKey(subtask => subtask.TaskId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
