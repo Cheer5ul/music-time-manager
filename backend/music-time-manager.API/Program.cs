d11ecbd5825792506e9073b742859060d0ef7797
Cheerful <yura08az@gmail.com>
2026-07-13 22:09:15 +0300

    feat: add database initializing

--- backend/music-time-manager.API/Program.cs
+++ backend/music-time-manager.API/Program.cs
@@ -1,10 +1,32 @@
+using music_time_manager.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Connecting db
+builder.Services.AddPersistence(builder.Configuration);
+
 var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var dbContext = serviceProvider.GetRequiredService<MusicTimeManagerDbContext>();
+        await DbInitializer.InitializeAsync(dbContext);
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex, ex.Message);
+        throw;
+    }
+}
 
 if (app.Environment.IsDevelopment())
 {
