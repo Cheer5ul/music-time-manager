80ddf1dc34636a1a4fc817bf3d66be09227e8696
Cheerful <yura08az@gmail.com>
2026-07-14 23:56:55 +0300

    wip: add UserRepository

---
+++ backend/music-time-manager.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace music_time_manager.Persistence.Repositories;
+
+public class UserRepository
+{
+    private readonly MusicTimeManagerDbContext _dbContext;
+    public UserRepository(MusicTimeManagerDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    
+}
