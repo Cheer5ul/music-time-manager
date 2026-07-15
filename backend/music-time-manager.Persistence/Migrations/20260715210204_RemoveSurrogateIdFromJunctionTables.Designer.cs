fd0836a23e7ff8fd8be07a7baf9c105ed2e86c1e
Cheerful <yura08az@gmail.com>
2026-07-16 00:03:01 +0300

    fix: remove surrogate id from db and migrate

--- backend/music-time-manager.Persistence/Migrations/20260715210204_RemoveSurrogateIdFromJunctionTables.Designer.cs
+++ backend/music-time-manager.Persistence/Migrations/20260715210204_RemoveSurrogateIdFromJunctionTables.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using music_time_manager.Persistence;
@@ -11,9 +12,11 @@ using music_time_manager.Persistence;
 namespace music_time_manager.Persistence.Migrations
 {
     [DbContext(typeof(MusicTimeManagerDbContext))]
-    partial class MusicTimeManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260715210204_RemoveSurrogateIdFromJunctionTables")]
+    partial class RemoveSurrogateIdFromJunctionTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/music-time-manager.Persistence/Migrations/20260715210204_RemoveSurrogateIdFromJunctionTables.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace music_time_manager.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveSurrogateIdFromJunctionTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "TaskAssignees");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "SubtaskAssignees");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "TaskAssignees",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "SubtaskAssignees",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
