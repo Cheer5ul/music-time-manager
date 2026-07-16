5cc1ff86aa74862c409f0503fdf158a04fa839ca
Cheerful <yura08az@gmail.com>
2026-07-16 23:55:30 +0300

    wip: add auth contoller

---
+++ backend/music-time-manager.API/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using music_time_manager.Application.DTOs;
+using music_time_manager.Application.Services;
+using SneakerStore.FailureHandler;
+
+namespace music_time_manager.API.Controllers;
+
+[ApiController]
+[Route("auth")]
+public class AuthController : ControllerBase
+{
+    private readonly IUserService _userService;
+    private readonly IFailureHandler _failureHandler;
+    
+    public AuthController(IUserService userService, 
+        IFailureHandler failureHandler)
+    {
+        _userService = userService;
+        _failureHandler = failureHandler;
+    }
+    
+    [HttpPost("register")]
+    public async Task<ActionResult> Register([FromBody] CreateUserDto createUserDto,
+        CancellationToken ct)
+    {
+        var result = await _userService.Create(createUserDto, ct);
+        
+        if(result.IsFailure) return _failureHandler.HandleFailure(result, HttpContext);
+        
+        return Ok();
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult> Login()
+    {
+        return Ok();
+    }
+}
