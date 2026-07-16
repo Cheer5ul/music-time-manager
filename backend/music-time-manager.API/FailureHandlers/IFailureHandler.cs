6922076018672ba9014418ac171ee3cf65881f46
Cheerful <yura08az@gmail.com>
2026-07-16 22:44:07 +0300

    feat: add failure handler

---
+++ backend/music-time-manager.API/FailureHandlers/IFailureHandler.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using music_time_manager.Core.Result;
+
+namespace SneakerStore.FailureHandler;
+
+public interface IFailureHandler
+{
+    ActionResult HandleFailure(Result result, HttpContext httpContext);
+}
