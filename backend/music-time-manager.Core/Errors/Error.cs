4c77120897d7422659431b23e2c1474128ab6419
Cheerful <yura08az@gmail.com>
2026-07-16 22:43:32 +0300

    chore: rename variable name from message to description

--- backend/music-time-manager.Core/Errors/Error.cs
+++ backend/music-time-manager.Core/Errors/Error.cs
@@ -2,14 +2,14 @@
 
 public sealed class Error
 {
-    public Error(string code, string message)
+    public Error(string code, string description)
     {
         Code = code;
-        Message = message;
+        Description = description;
     }
     
     public string Code { get; }
-    public string Message { get; }
+    public string Description { get; }
     
     public static Error None => new Error(string.Empty, string.Empty); 
 }
