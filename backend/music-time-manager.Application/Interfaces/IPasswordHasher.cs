f96155c5bb4d77bf83d40ff662560e28a072e947
Cheerful <yura08az@gmail.com>
2026-07-17 12:49:00 +0300

    feat: add PasswordHasher & DI

---
+++ backend/music-time-manager.Application/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace music_time_manager.Infrastructure;
+
+public interface IPasswordHasher
+{
+    string Generate(string password);
+    bool Verify(string password, string hashedPassword);
+}
---
+++ backend/music-time-manager.Infrastructure/PasswordHasher.cs
@@ -0,0 +1,10 @@
+﻿namespace music_time_manager.Infrastructure;
+
+public class PasswordHasher : IPasswordHasher
+{
+    public string Generate(string password) => 
+        BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+    
+    public bool Verify(string password, string hashedPassword) =>
+        BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword);
+}
