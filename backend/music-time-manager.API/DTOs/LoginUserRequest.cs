ce79ab904a819de2e1c441c40f784c7bab139a75
Cheerful <yura08az@gmail.com>
2026-07-17 13:01:20 +0300

    feat: dto to login user

---
+++ backend/music-time-manager.API/DTOs/LoginUserRequest.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace music_time_manager.Application.DTOs;
+
+public record LoginUserRequest(
+    [Required] string Username,
+    [Required] string Password);
