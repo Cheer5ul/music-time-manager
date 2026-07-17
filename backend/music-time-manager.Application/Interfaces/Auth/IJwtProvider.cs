94e15a63b78059cc764e6ad69d8b4106216e86bc
Cheerful <yura08az@gmail.com>
2026-07-17 13:39:47 +0300

    feat: add JwtProvider & DI

---
+++ backend/music-time-manager.Application/Interfaces/Auth/IJwtProvider.cs
@@ -0,0 +1,8 @@
+﻿using music_time_manager.Core.Models;
+
+namespace music_time_manager.Infrastructure;
+
+public interface IJwtProvider
+{
+    string GenerateToken(User user);
+}
---
+++ backend/music-time-manager.Infrastructure/Providers/JwtProvider.cs
@@ -0,0 +1,36 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using music_time_manager.Core.Models;
+using music_time_manager.Infrastructure.Options;
+
+namespace music_time_manager.Infrastructure;
+
+public class JwtProvider : IJwtProvider
+{
+    private readonly JwtOptions _jwtOptions;
+    public JwtProvider(IOptions<JwtOptions> options)
+    {
+        _jwtOptions = options.Value;
+    }
+    
+    public string GenerateToken(User user)
+    {
+        Claim[] claims = [new("userId", user.Id.ToString())];
+        
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.SecretKey)), 
+            SecurityAlgorithms.HmacSha256);
+        
+        var token = new JwtSecurityToken(
+            claims: claims,
+            signingCredentials: signingCredentials,
+            expires: DateTime.UtcNow.AddHours(_jwtOptions.ExpiresHours));
+        
+        var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+        
+        return tokenValue;
+    }
+}
