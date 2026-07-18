11b3c05e9b0a2316e1b0158c25d9da61de1e25fe
Cheerful <yura08az@gmail.com>
2026-07-18 21:45:39 +0300

    wip: create task controller

---
+++ backend/music-time-manager.API/Controllers/TaskController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using music_time_manager.Application.DTOs;
+using music_time_manager.Application.Services;
+using music_time_manager.Core.Models;
+using SneakerStore.FailureHandler;
+using Task = music_time_manager.Core.Models.Task;
+
+namespace music_time_manager.API.Controllers;
+
+[ApiController]
+[Route("tasks")]
+public class TaskController : ControllerBase
+{
+    private readonly ITaskService _taskService;
+    private readonly IFailureHandler _failureHandler;
+    public TaskController(ITaskService taskService,
+        IFailureHandler failureHandler)
+    {
+        _taskService = taskService;
+        _failureHandler = failureHandler;
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult> CreateTask(
+        [FromBody] TaskRequest request,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await _taskService.CreateTask(
+            request.Title,
+            request.DueDate,
+            request.CreatedBy,
+            request.Description, 
+            cancellationToken);
+        
+        if(result.IsFailure) return _failureHandler.HandleFailure(result, HttpContext);
+        
+        return Ok(result);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> DeleteTask(Guid id, CancellationToken ct = default)
+    {
+        var result = await _taskService.Delete(id, ct);
+        
+        if(result.IsFailure) return _failureHandler.HandleFailure(result, HttpContext);
+            
+        return Ok(result);
+    }
+}
