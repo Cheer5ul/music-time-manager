2e3e83c47dc9343c65db21ec8d7699f6c07f5dee
Cheerful <yura08az@gmail.com>
2026-07-18 21:43:37 +0300

    wip: add TasService & DI

---
+++ backend/music-time-manager.Application/Interfaces/Services/ITaskService.cs
@@ -0,0 +1,17 @@
+﻿using music_time_manager.Core.Models;
+using music_time_manager.Core.Result;
+using Task = music_time_manager.Core.Models.Task;
+
+namespace music_time_manager.Application.Services;
+
+public interface ITaskService
+{
+    Task<Result> CreateTask(string title, DateTime dueDate,
+        Guid createdBy, string? description, CancellationToken ct = default);
+    Task<Result> CreateTaskWithAssignees(Task task, List<Guid> taskAssignedUsersIds,
+        List<Guid> subtaskAssignedUsersIds,
+        CancellationToken ct = default);
+
+    Task<Result> Delete(Guid taskId,
+        CancellationToken ct = default);
+}
---
+++ backend/music-time-manager.Application/Services/TaskService.cs
@@ -0,0 +1,76 @@
+﻿using System.Globalization;
+using music_time_manager.Core.Errors;
+using music_time_manager.Core.Models;
+using music_time_manager.Core.Result;
+using music_time_manager.Persistence.Repositories;
+using Task = music_time_manager.Core.Models.Task;
+
+namespace music_time_manager.Application.Services;
+
+public class TaskService : ITaskService
+{
+    private readonly ITaskRepository _taskRepository;
+    
+    public TaskService(ITaskRepository repository)
+    {
+        _taskRepository = repository;
+    }
+
+    public async Task<Result> CreateTask(string title, DateTime dueDate,
+        Guid createdBy, string? description, CancellationToken ct = default)
+    {
+        var task = Task.Create(
+            title,
+            dueDate,
+            createdBy,
+            new List<Subtask>(),
+            description,
+            null);
+        
+        if(task.IsFailure) return Result.Failures(task.Errors);
+        
+        await  _taskRepository.CreateTask(task.Value!, ct);
+        return Result.Success;
+    }
+    
+    public async Task<Result> CreateTaskWithAssignees(Task task, List<Guid> taskAssignedUsersIds, 
+        List<Guid> subtaskAssignedUsersIds,
+        CancellationToken ct = default)
+    {
+        // task assignees
+        List<TaskAssignee> taskAssignees = [];
+
+        foreach (var assigneeId in taskAssignedUsersIds)
+        {
+            taskAssignees.Add(
+                TaskAssignee.Reconstitute(task.Id, assigneeId));
+        }
+        
+        // subtask assignees
+        List<SubtaskAssignee> subtaskAssignees = [];
+
+        foreach (var assigneeId in subtaskAssignedUsersIds)
+        {
+            foreach (var subtask in task.SubTasks)
+            {
+                subtaskAssignees.Add(
+                    SubtaskAssignee.Reconstitute(subtask.TaskId, assigneeId));
+            }
+        }
+        
+        await _taskRepository.CreateTaskWithAssignees(task.Id, taskAssignees, subtaskAssignees, ct);
+        
+        return Result.Success; 
+    }
+
+    public async Task<Result> Delete(Guid taskId,
+        CancellationToken ct = default)
+    {
+        var doesTaskExist = await _taskRepository.DoesTaskExist(taskId, ct);
+
+        if (!doesTaskExist) return Result.Failures([TaskErrors.DoesNotExist(taskId)]);
+        
+        await _taskRepository.DeleteTask(taskId, ct);
+        return Result.Success;
+    }
+}
