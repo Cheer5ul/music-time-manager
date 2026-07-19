765ba9981d31c89a649ae8c318519ebe07697810
Cheerful <yura08az@gmail.com>
2026-07-19 21:16:08 +0300

    feat: add subtask response

---
+++ backend/music-time-manager.API/DTOs/Response/SubtaskResponse.cs
@@ -0,0 +1,9 @@
+﻿using CoreStatus = music_time_manager.Core.Models.Status;
+namespace music_time_manager.API.DTOs;
+
+public record SubtaskResponse(
+    Guid Id,
+    string Title,
+    CoreStatus Status,
+    // bool IsOverdue,
+    Guid TaskId);
