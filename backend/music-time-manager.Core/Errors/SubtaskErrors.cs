c874b8b32bf9612481a8a29337011a202a5a8d18
Cheerful <yura08az@gmail.com>
2026-07-20 21:05:52 +0300

    feat: add more errors

--- backend/music-time-manager.Core/Errors/SubtaskErrors.cs
+++ backend/music-time-manager.Core/Errors/SubtaskErrors.cs
@@ -4,10 +4,19 @@ public static class SubtaskErrors
 {
     private static class Codes
     {
-        public const string InvalidTitle = "Task.InvalidTtile";
+        public const string InvalidTitle = "Subtask.InvalidTtile";
+        public const string MustHaveAtLeastOneAssignee = "Subtask.MustHaveAtLeastOneAssignee";
+        public const string DoesNotExist = "Subtask.DoesNotExist";
     } 
     
     public static Error InvalidTitle(string title) =>
         new Error(Codes.InvalidTitle, 
             $"Title '{title}' is invalid.");
+    public static Error MustHaveAtLeastOneAssignee() =>
+        new Error(Codes.MustHaveAtLeastOneAssignee,
+            $"Task must have at least one assignee.");
+
+    public static Error DoesNotExist(Guid id) =>
+        new Error(Codes.DoesNotExist,
+            $"Subtask with id {id} does not exist.");
 }
