34a184d901d18ef6357dd2b5d1803c441242bb9b
Cheerful <yura08az@gmail.com>
2026-07-21 23:33:34 +0300

    wip: add AddApiAuthentication method with Jwt Token configurations

---
+++ backend/music-time-manager.API/Extensions/ApiExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using music_time_manager.Infrastructure.Options;
+
+namespace music_time_manager.API.Extensions;
+
+public static class ApiExtensions
+{
+    public static void AddApiAuthentication(
+        IServiceCollection services,
+        IConfiguration configuration,
+        IOptions<JwtOptions> jwtOptions)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtOptions.Value.SecretKey)),
+                };
+            });
+
+        services.AddAuthorization();
+    }
+}
