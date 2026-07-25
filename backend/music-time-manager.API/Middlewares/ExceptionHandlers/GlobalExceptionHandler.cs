3bb255e36cb5f97a3507564ab27c724d041ec4cd
Cheerful <yura08az@gmail.com>
2026-07-25 17:02:52 +0300

    feat: add global exception handler

---
+++ backend/music-time-manager.API/Middlewares/ExceptionHandlers/GlobalExceptionHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+
+namespace music_time_manager.API.Middlewares.ExceptionHandlers;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger;
+    private readonly IProblemDetailsService _problemDetailsService;
+
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger,
+        IProblemDetailsService problemDetailsService)
+    {
+        _logger = logger;
+        _problemDetailsService = problemDetailsService;
+    }
+    
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext, 
+        Exception exception, 
+        CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "Unhandled exception occurred");
+
+        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        return await _problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+        {
+            HttpContext = httpContext,
+            Exception = exception,
+            ProblemDetails = new ProblemDetails()
+            {
+                Type = exception.GetType().Name,
+                Title = "An error occurred",
+                Instance = $"{httpContext.Request.Method} {httpContext.Request.Path}"
+            }
+        });
+    }
+}
