4c02645e7b7c7d876b6b14375e7a97fb24e46630
Cheerful <yura08az@gmail.com>
2026-08-03 22:57:08 +0300

    feat: add helper to get user id from claimsPrincipal

---
+++ backend/music-time-manager.API/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace music_time_manager.API.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static Guid? GetUserId(this ClaimsPrincipal principal)
+    {
+        var claim = principal.FindFirstValue("userId");
+        return Guid.TryParse(claim, out var id) ? id : null;
+    }
+}
