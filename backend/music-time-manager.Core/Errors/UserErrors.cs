310c74ada1d0bc96c4eda638dd646bdb5b2e16c1
Cheerful <yura08az@gmail.com>
2026-08-05 23:06:34 +0300

    refactor: use ErrorType

--- backend/music-time-manager.Core/Errors/UserErrors.cs
+++ backend/music-time-manager.Core/Errors/UserErrors.cs
@@ -13,21 +13,27 @@ public static class UserErrors
     }
     public static Error InvalidUsername(string username) => 
         new Error(Codes.InvalidUsername,
-            $"Username '{username}' is invalid."); 
+            $"Username '{username}' is invalid.",
+            ErrorType.Validation); 
     public static Error NotFoundName(string username) => 
         new Error(Codes.NotFoundName,
-            $"User with username '{username}' is not found.");
+            $"User with username '{username}' is not found.",
+            ErrorType.NotFound);
     public static Error NotFound()
         => new Error(Codes.NotFound,
-            $"User is not found.");
+            $"User is not found.",
+            ErrorType.NotFound);
     public static Error FailedToLogin() => 
         new Error(Codes.FailedToLogin,
-            $"Failed to login.");
+            $"Failed to login.",
+            ErrorType.Validation);
     public static Error NameAlreadyUsed(string name) =>
         new Error(Codes.NameAlreadyUsed,
-            $"User with name '{name}' already exists.");
+            $"User with name '{name}' already exists.",
+            ErrorType.Conflict);
 
     public static Error DoesNotExits(Guid id) =>
         new Error(Codes.DoesNotExist,
-            $"User with id {id} does not exist.");
+            $"User with id {id} does not exist.",
+            ErrorType.NotFound);
 }
