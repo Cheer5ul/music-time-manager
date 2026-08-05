4ad679f89cf092baa8e0b8d3499ad363e8f7c17b
Cheerful <yura08az@gmail.com>
2026-08-05 22:53:22 +0300

    refactor: use ErrorType

--- backend/music-time-manager.Core/Errors/Error.cs
+++ backend/music-time-manager.Core/Errors/Error.cs
@@ -2,14 +2,16 @@
 
 public sealed class Error
 {
-    public Error(string code, string description)
+    public Error(string code, string description, ErrorType type)
     {
         Code = code;
         Description = description;
+        Type = type;
     }
     
     public string Code { get; }
     public string Description { get; }
+    public ErrorType Type { get; }
     
-    public static Error None => new Error(string.Empty, string.Empty); 
+    // public static Error None => new Error(string.Empty, string.Empty); 
 }
