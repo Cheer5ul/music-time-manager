f4cab2c35df5cdab70b74f4459557d65a712ca56
Cheerful <yura08az@gmail.com>
2026-08-06 00:09:48 +0300

    feat: add dto for task recreating

---
+++ backend/music-time-manager.API/DTOs/Request/TaskRecreateRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace music_time_manager.Application.DTOs;
+
+public record TaskRecreateRequest(
+    [Required] DateTime DueDate,
+    [MaxLength(200)] string? Title, 
+    [MaxLength(2000)] string? Description,
+    [MinLength(1)] List<Guid>? AssigneeIds
+    );
