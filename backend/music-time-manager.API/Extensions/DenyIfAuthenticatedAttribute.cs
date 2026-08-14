e7171fa7a44835f0779fab8ea469f76223d75b50
Cheerful <yura08az@gmail.com>
2026-08-14 22:47:51 +0300

    feat: add attribute to deny requests if user already logged in

---
+++ backend/music-time-manager.API/Extensions/DenyIfAuthenticatedAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace music_time_manager.API.Extensions;
+
+public class DenyIfAuthenticatedAttribute : Attribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        if (context.HttpContext.User.Identity?.IsAuthenticated == true)
+        {
+            context.Result = new ConflictObjectResult("You are already logged in.");
+        }
+    }
+}
